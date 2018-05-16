e6b34ba17ff3649dc0dcb157187ad05f687e80a9
Allen Tang <allentang2018@u.northwestern.edu>
2018-05-16 09:28:38 -0500

    experimented with url

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from urllib import request
-import Image
-import sys, io, os
+import requests
+from bing_tile import TileSystem
+from PIL import Image
+import sys, io, os, time
 
 if __name__ == '__main__':
+    # Request base url
+    r = requests.get("http://dev.virtualearth.net/REST/V1/Imagery/Metadata/Road?output=json&include=ImageryProviders&key=AuL4N1YXVUkjZYd0uY9LKs4z1TluN-6GAZY9VKEj6zVSP_Q402Dm37QWPiG_0b06')
     baseUrl = "http://h0.ortho.tiles.virtualearth.net/tiles/h{0}.jpeg?g=131"
     quadKey = "023131022213211200"
-    with request.urlopen(baseUrl.format(quadkey)) as file:
+    with request.urlopen(baseUrl.format(quadKey)) as file:
         Image.open(file)
