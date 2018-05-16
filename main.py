63cc228705b601671e100cac1563315e5dad3bc1
Da Yeon Hwang <dahwang2018@u.northwestern.edu>
2018-05-15 22:03:50 -0500

    bing map request working

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from urllib import request
-import Image
+import requests
+from PIL import Image
 import sys, io, os
+import json
+import pprint
 
 if __name__ == '__main__':
-    baseUrl = "http://h0.ortho.tiles.virtualearth.net/tiles/h{0}.jpeg?g=131"
-    quadKey = "023131022213211200"
-    with request.urlopen(baseUrl.format(quadkey)) as file:
-        Image.open(file)
+    baseUrl = "https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial?key="
+    bingKey = "AuL4N1YXVUkjZYd0uY9LKs4z1TluN-6GAZY9VKEj6zVSP_Q402Dm37QWPiG_0b06"
+    r = requests.get(baseUrl+bingKey)
+    j = r.json()
+    #pprint.pprint(j)
+    imageUrl = j['resourceSets'][0]['resources'][0]['imageUrl']
+    print (imageUrl)
